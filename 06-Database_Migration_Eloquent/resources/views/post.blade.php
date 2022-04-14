fcafb6caa301078df9ced63bad9b8f9ffc78c2f8
AcilRestu12 <arieskarestu214@gmail.com>
2022-04-15 00:19:31 +0700

    06-Database_Migration_Eloquent

---
+++ 06-Database_Migration_Eloquent/resources/views/post.blade.php
@@ -0,0 +1,15 @@
+{{-- Mengextends halaman main.blade.php --}}
+@extends('layouts.main')
+
+{{-- Mengisi halaman parent untuk section container --}}
+@section('container')
+    <article>
+        {{-- Menerima data post yg dikirim dari route --}}
+        <h2>{{ $post['title'] }}</h2>
+        <h5>{{ $post['author'] }}</h5>
+        <p>{{ $post['body'] }}</p>
+    </article>
+
+    <a href="/posts">Back to Posts</a>
+@endsection
+
