20962da4fb84931f970bc26d6c2fd24b15786c02
AcilRestu12 <arieskarestu214@gmail.com>
2022-04-27 02:00:15 +0700

    part 20 start

---
+++ 20-Update_and_Delete_Post/app/Http/Controllers/DashboardPostController.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+
+use \Cviebrock\EloquentSluggable\Services\SlugService;
+use Illuminate\Support\Str;
+
+
+class DashboardPostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+    */
+
+    //  Method untuk menampilkan semua data post berdasarkan user tertentu
+    public function index()
+    {
+        // Memanggil file view dashboard/posts/index.blade.php dan mengirim data posts
+        return view('dashboard.posts.index', [
+            'posts' => Post::where('user_id', auth()->user()->id)->get()        // Mengambil data posts berdasarkan id user yang sudah login
+        ]);
+    }
+
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+    */
+
+    //  Method untuk menampilkan halaman tambah post
+    public function create()
+    {
+        // Memanggil file view dashboard/posts/create.blade.php
+        return view('dashboard.posts.create', [
+            'categories' => Category::all()     // Mengirimkan data categories
+        ]);
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+    */
+
+    // Method untuk menjalankan fungsi tambah post
+    public function store(Request $request)
+    {
+        // Melakukan validasi data untuk setiap data request
+        $validatedData = $request->validate([
+            'title' => 'required|max:255',
+            'slug' => 'required|unique:posts',
+            'category_id' => 'required',
+            'body' => 'required'
+        ]);
+
+        // Menambahkan data id user ke variabel validatedData dengan mengambil dari auth()
+        $validatedData['user_id'] = auth()->user()->id;
+
+        // Menambahkan data excerpt ke variabel validatedData dengan mengambil 200 kata dari body
+        $validatedData['excerpt'] = Str::limit(strip_tags($request->body), 200);        // strip_tags() ~> Berfungsi untuk menghilangkan tag-tag html
+        
+        // Memasukkan data post ke tabel post 
+        Post::create($validatedData);
+
+        // Mengirim flash message dan meredirect url ke halaman /dashboard/posts
+        return redirect('/dashboard/posts')->with('success', 'New post has been added!');
+        
+    }
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+    */
+
+    // Method untuk menampilkan detail dari post
+    public function show(Post $post)
+    {
+        // Memanggil file view dashboard/posts/show.blade.php dan mengirim data post
+        return view('dashboard.posts.show', [
+            'post' => $post
+        ]);
+    }
+
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+    */
+
+    // Method untuk menampilkan ubah data post
+    public function edit(Post $post)
+    {
+        //
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+    */
+    
+    // Method untuk memproses perubahan data post
+    public function update(Request $request, Post $post)
+    {
+        //
+    }
+
+    
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+    */
+
+    // Method untuk menghapus data post
+    public function destroy(Post $post)
+    {
+        //
+    }
+
+
+    // Method untuk menangani ketika ada perminataan slug
+    public function checkSlug(Request $request) {
+
+        // Membuat slug dari title yg diambil dari request url
+        $slug = SlugService::createSlug(Post::class, 'slug', $request->title);
+
+        // Mereturn slug sebagai response dalam bentuk json
+        return response()->json(['slug' => $slug]);
+        
+    }
+
+
+    
+    
+}
