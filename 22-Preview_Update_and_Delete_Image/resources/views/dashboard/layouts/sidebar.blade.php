78189f4ab70cb650e077bab448c1ed5f8ce6441b
AcilRestu12 <arieskarestu214@gmail.com>
2022-04-28 23:51:49 +0700

    part 22 start

---
+++ 22-Preview_Update_and_Delete_Image/resources/views/dashboard/layouts/sidebar.blade.php
@@ -0,0 +1,21 @@
+<nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+    <div class="position-sticky pt-3">
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                {{-- Apabila ada request url dasboard maka tambahkan class active --}}
+                <a class="nav-link {{ Request::is('dashboard') ? 'active' : '' }}" aria-current="page" href="/dashboard">       
+                    <span data-feather="home"></span>
+                    Dashboard
+                </a>
+            </li>
+            <li class="nav-item">
+                {{-- Link untuk mengarahkan url ke route /dashboard/posts --}}
+                {{-- Apabila ada request url dashboard/posts/... maka tambahkan class active --}}
+                <a class="nav-link {{ Request::is('dashboard/posts*') ? 'active' : '' }}" href="/dashboard/posts">
+                    <span data-feather="file-text"></span>
+                    My Posts
+                </a>
+            </li>
+        </ul>
+    </div>
+</nav>
